b718a15436ca34cefa0b7a3f63a640614d7e3682
konrod9 <rodion.kondrashin@bk.ru>
2026-09-09 17:17:11 +0700

    Add API layer

---
+++ src/ChoiceHotels.API/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
